2aa805f4e7f0d284a1dcfee3e5b4f81175c6f1f5
Subharghya Manna <subharghya.manna.20@gmail.com>
2024-10-05 11:44:42 +0530

    Bug Fix

--- 03.SpringDataJPA/13.SpringBootDataJPAUtil/src/main/java/com/springproject/sequence/EnquirySequenceGenerator.java
+++ 03.SpringDataJPA/13.SpringBootDataJPAUtil/src/main/java/com/springproject/sequence/EnquirySequenceGenerator.java
@@ -13,13 +13,29 @@ public class EnquirySequenceGenerator implements IdentifierGenerator {
 
 	private static final long serialVersionUID = 1L;
 
+	private void init(SharedSessionContractImplementor session) {
+
+		// Create a table if not available
+		String query1 = "CREATE TABLE ENQ_SEQ(NEXTVAL INT)";
+
+		// set the default value of the NEXTVAL as 0 or 1
+		String query2 = "INSERT INTO ENQ_SEQ (NEXTVAL) VALUES (1)";
+
+		Connection con;
+		Statement st;
+		try {
+			con = session.getJdbcConnectionAccess().obtainConnection();
+			st = con.createStatement();
+			st.executeUpdate(query1);
+			st.executeUpdate(query2);
+		} catch (SQLException e) {
+			// ignore
+		}
+
+	}
+
 	@Override
 	public Serializable generate(SharedSessionContractImplementor session, Object object) {
-
-		static {
-			// Create a table if not available
-			// set the default value as 0 or 1
-		}
 
 		String prefix = "OD";
 		String suffix = null;
@@ -27,15 +43,17 @@ public class EnquirySequenceGenerator implements IdentifierGenerator {
 		// Get the next seq value
 		try {
 
+			init(session);
+
 			String query1 = "SELECT NEXTVAL FROM ENQ_SEQ";
-			String query2 = "UPDATE ENQ_SEQ SET NEXTVAL=NEXTVAL+1";
+			String query2 = "UPDATE ENQ_SEQ SET NEXTVAL=NEXTVAL+1"; // increment the value
 
 			Connection con = session.getJdbcConnectionAccess().obtainConnection();
 			Statement st = con.createStatement();
 
 			ResultSet rs = st.executeQuery(query1);
 			if (rs.next()) {
-				suffix = rs.getString(1);
+				suffix = String.format("%010d", rs.getInt(1));
 			}
 
 			st.executeUpdate(query2);
